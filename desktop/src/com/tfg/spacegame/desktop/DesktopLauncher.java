f46533d2644d42568cc9452aeb84e4fe2c679482
tonigallego <tonigallego87@gmail.com>
2015-11-10 13:33:07 +0100

    Subida del proyecto
    Signed-off-by: tonigallego <tonigallego87@gmail.com>

---
+++ desktop/src/com/tfg/spacegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tfg.spacegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tfg.spacegame.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}
