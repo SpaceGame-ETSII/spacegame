24b1d450d2d73ab38f300b3bddcf5e156eeda96e
DrXoo <domgromero@gmail.com>
2015-11-10 14:09:42 +0100

    Añadida la configuación (resolución y título) de escritorio en el Launcher del juego.

--- desktop/src/com/tfg/spacegame/desktop/DesktopLauncher.java
+++ desktop/src/com/tfg/spacegame/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.tfg.spacegame.SpaceGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		config.title = "Space Game";
 		new LwjglApplication(new SpaceGame(), config);
 	}
 }
