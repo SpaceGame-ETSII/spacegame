2d072784479963572219d825b01cc024f3023086
Joseda13 <joseda.martin@gmail.com>
2016-01-20 13:59:04 +0100

    Añadido pulsamiento de botones
    Se han añadido que al pulsar un botón del menú principal, éste se vea pulsado y transurridos unos segundos efectue el efecto deseado.

--- core/src/com/tfg/spacegame/screens/MainMenuScreen.java
+++ core/src/com/tfg/spacegame/screens/MainMenuScreen.java
@@ -3,16 +3,24 @@ package com.tfg.spacegame.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 import com.tfg.spacegame.GameObject;
 import com.tfg.spacegame.SpaceGame;
 
 public class MainMenuScreen implements Screen {
+
     GameObject campaing;
     GameObject arcade;
     GameObject multi;
     GameObject options;
     GameObject exit;
+
+    private float timeUntilExit;
+    private boolean pulseCampaing=false;
+    private boolean pulseArcade=false;
+    private boolean pulseMulti=false;
+    private boolean pulseOptions=false;
+    private boolean pulseExit=false;
 
     final SpaceGame game;
 
@@ -22,11 +30,11 @@ public class MainMenuScreen implements Screen {
     }
 
     public void create(){
-        campaing = new GameObject("button",255,250);
-        arcade = new GameObject("button",255,190);
-        multi = new GameObject("button",255,130);
-        options = new GameObject("button",255,70);
-        exit = new GameObject("button",255,10);
+        campaing = new GameObject("button",290,315);
+        arcade = new GameObject("button",290,255);
+        multi = new GameObject("button",290,195);
+        options = new GameObject("button",290,135);
+        exit = new GameObject("button",290,75);
     }
 
     @Override
@@ -41,11 +49,32 @@ public class MainMenuScreen implements Screen {
         // Pintamos el fondo
         game.batch.draw(game.background, 0,0);
         // Pintamos cada uno de los botones
-        campaing.render(game.batch);
-        arcade.render(game.batch);
-        multi.render(game.batch);
-        options.render(game.batch);
-        exit.render(game.batch);
+        if (pulseCampaing){
+            campaing.renderRotate(game.batch,180);
+        }else{
+            campaing.render(game.batch);
+        }
+        if(pulseArcade){
+            arcade.renderRotate(game.batch,180);
+        }else {
+            arcade.render(game.batch);
+        }
+        if(pulseMulti){
+            multi.renderRotate(game.batch,180);
+        }else {
+            multi.render(game.batch);
+        }
+        if(pulseOptions){
+            options.renderRotate(game.batch,180);
+        }else{
+            options.render(game.batch);
+        }
+        if(pulseExit){
+            exit.renderRotate(game.batch, 180);
+        }else {
+            exit.render(game.batch);
+        }
+
         // Pintamos los títulos de los botontes
         game.font.draw(game.batch, "SPACE GAME", 350, 400);
         game.font.draw(game.batch, "Modo Campaña", 345, 345);
@@ -54,15 +83,62 @@ public class MainMenuScreen implements Screen {
         game.font.draw(game.batch, "Opciones", 365, 165);
         game.font.draw(game.batch, "Salir", 380, 105);
 
+        updateButton(delta,new GameScreen(game),pulseCampaing);
+        updateButton(delta,new ArcadeScreen(game),pulseArcade);
+        updateButton(delta,new MultijugadorScreen(game),pulseMulti);
+        updateButton(delta,new OptionsScreen(game),pulseOptions);
+        updateExit(delta,pulseExit);
+
         game.batch.end();
 
-        /*if (Gdx.input.isTouched()) {
-            game.setScreen(new GameScreen(game));
-            dispose();
-        }*/
+        if (Gdx.input.justTouched()) {
+
+            Vector3 v = new Vector3(Gdx.input.getX(),Gdx.input.getY(),0);
+            v = game.camera.unproject(v);
+
+            if(campaing.isOverlapingWith(v.x, v.y)){
+                timeUntilExit=0.5f;
+                pulseCampaing=true;
+            }
+            if(arcade.isOverlapingWith(v.x, v.y)) {
+                timeUntilExit=0.5f;
+                pulseArcade=true;
+            }
+            if(multi.isOverlapingWith(v.x, v.y)){
+                timeUntilExit=0.5f;
+                pulseMulti=true;
+            }
+            if(options.isOverlapingWith(v.x, v.y)){
+                timeUntilExit=0.5f;
+                pulseOptions=true;
+            }
+            if(exit.isOverlapingWith(v.x, v.y) ){
+                timeUntilExit=0.5f;
+                pulseExit=true;
+            }
+        }
+
     }
 
-	@Override
+    public void updateButton(float delta,Screen screen,boolean ex){
+        if(timeUntilExit<=0 && ex){
+            game.setScreen(screen);
+            dispose();
+        }else{
+            timeUntilExit-=delta;
+        }
+    }
+
+    public void updateExit(float delta,boolean ex){
+        if(timeUntilExit<=0 && ex){
+            Gdx.app.exit();
+            dispose();
+        }else{
+            timeUntilExit-=delta;
+        }
+    }
+
+    @Override
 	public void show() {
 		// TODO Auto-generated method stub
 		
@@ -95,7 +171,7 @@ public class MainMenuScreen implements Screen {
         campaing.dispose();
         arcade.dispose();
         multi.dispose();
-        arcade.dispose();
+        options.dispose();
         exit.dispose();
 	}
 
---
+++ core/src/com/tfg/spacegame/screens/MultijugadorScreen.java
@@ -0,0 +1,66 @@
+package com.tfg.spacegame.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.tfg.spacegame.SpaceGame;
+
+public class MultijugadorScreen implements Screen {
+
+    final SpaceGame game;
+
+    public MultijugadorScreen(final SpaceGame gam) {
+        game = gam;
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        game.camera.update();
+        game.batch.setProjectionMatrix(game.camera.combined);
+
+        game.batch.begin();
+        // Pintamos el fondo
+        game.batch.draw(game.background, 0,0);
+
+        game.font.draw(game.batch, "MODO MULTIJUGADOR", 350, 400);
+
+        game.batch.end();
+    }
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+        // TODO Auto-generated method stub
+	}
+
+}
