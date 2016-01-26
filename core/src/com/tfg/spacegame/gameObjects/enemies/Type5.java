469dcdc0ea3dab3f3c64cdd60d219247db0baba5
Joseda13 <joseda.martin@gmail.com>
2016-01-26 16:48:00 +0100

    Aparación del enemigo básico tipo 5
    Se ha codificado la aparicón del enemigo básico tipo 5, para que aparezca en el modo campaña nada más empezar.

--- core/src/com/tfg/spacegame/gameObjects/enemies/Type5.java
+++ core/src/com/tfg/spacegame/gameObjects/enemies/Type5.java
@@ -1,14 +1,26 @@
 package com.tfg.spacegame.gameObjects.enemies;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.tfg.spacegame.gameObjects.Enemy;
 
 public class Type5 extends Enemy{
 
-    public static final int SPEED = 200;
+    //Indica la velocidad a la que se moverá el enemigo hasta un punto fijo
+    public static final int SPEED = 50;
+    //Indicará el número de píxeles que deberá recorrer el enemigo hasta detenerse
+    private int pixelsToMove;
 
     public Type5(int x, int y) {
         super("enemigo_basico_tipo5", x, y);
+        pixelsToMove = 650;
     }
 
+    public void update(float delta){
+        //Mientras el enemigo tenga distancia que recorrer, vamos actualizando su posición
+        if (pixelsToMove > 0) {
+            this.setX(this.getX() - SPEED * delta);
+            pixelsToMove -= SPEED*delta;
+        }
+    }
 
 }
