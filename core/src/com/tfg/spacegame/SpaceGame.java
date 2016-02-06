f73b35ab74b376dbb1aac899456cea47304461f0
DrXoo <domgromero@gmail.com>
2016-02-07 00:55:52 +0100

    Añadida funcionalidad para obtener el punto de touch de forma estatica

--- core/src/com/tfg/spacegame/SpaceGame.java
+++ core/src/com/tfg/spacegame/SpaceGame.java
@@ -1,10 +1,13 @@
 package com.tfg.spacegame;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.tfg.spacegame.screens.MainMenuScreen;
 import com.tfg.spacegame.utils.AssetsManager;
 
@@ -13,9 +16,11 @@ public class SpaceGame extends Game {
 	public static SpriteBatch batch;
 	public static BitmapFont font;
 	public Texture background;
-	public OrthographicCamera camera;
+	public static OrthographicCamera camera;
 	public static int width = 800;
 	public static int height = 480;
+
+	private static Vector3 touchPos;
 
 	@Override
 	public void create () {
@@ -29,6 +34,8 @@ public class SpaceGame extends Game {
 		camera.setToOrtho(false, SpaceGame.width, SpaceGame.height);
 
 		this.setScreen(new MainMenuScreen(this));
+
+		touchPos = new Vector3();
 	}
 
 	public void render() {
@@ -38,5 +45,16 @@ public class SpaceGame extends Game {
 	public void dispose() {
 		batch.dispose();
 		font.dispose();
+	}
+
+	public static Vector3 getTouchPos(int point){
+		if(point == 0){
+			touchPos.set(Gdx.input.getX(),Gdx.input.getY(),0);
+		}else{
+			touchPos.set(Gdx.input.getX(point),Gdx.input.getY(point),0);
+		}
+		touchPos = camera.unproject(touchPos);
+
+		return touchPos;
 	}
 }
