e3b876e7cb9aed91e9cae86541703c7dc3071e21
DrXoo <domgromero@gmail.com>
2016-02-07 18:29:39 +0100

    Añadido temporizador al arma amarilla

--- core/src/com/tfg/spacegame/gameObjects/shoots/Yellow.java
+++ core/src/com/tfg/spacegame/gameObjects/shoots/Yellow.java
@@ -2,14 +2,9 @@ package com.tfg.spacegame.gameObjects.shoots;
 
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.ParticleEffect;
 import com.badlogic.gdx.graphics.g2d.ParticleEmitter;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.tfg.spacegame.GameObject;
 import com.tfg.spacegame.SpaceGame;
@@ -18,9 +13,20 @@ import com.tfg.spacegame.utils.AssetsManager;
 
 public class Yellow extends Shoot{
 
+    // Antes de eliminar el disparo esperamos un poco
+    private static final float FREQUENCY_OF_SHOOTING=0.7f;
+
+    // Amplitud del efecto de disparo (el efecto de particulas)
+    private static final int AMPLITUDE_OF_FIRE=15;
+
+    // Lo usaremos para hacer el calculo del ángulo
     private Vector2 vector;
 
+    // El efecto de disparo
     private ParticleEffect shootEffect;
+
+    // Tiempo para recargar
+    private float timeToFireAgain;
 
     public Yellow(GameObject shooter) {
         super("yellow_shoot", 0, 0, shooter);
@@ -33,14 +39,17 @@ public class Yellow extends Shoot{
 
         shootEffect.start();
 
+        timeToFireAgain = FREQUENCY_OF_SHOOTING;
+
         vector = new Vector2();
     }
 
     public void update(float delta){
-        this.setY(getShooter().getY() + this.getHeight());
+        setY(getShooter().getY() + this.getHeight());
         shootEffect.update(delta);
         shootEffect.getEmitters().first().setPosition(this.getX() - this.getHeight()/2, this.getY() + this.getHeight()/2);
 
+        // Controlamos si el jugador sigue queriendo disparar
         if(Gdx.input.isTouched(1) || (Gdx.input.isTouched(0) && SpaceGame.getTouchPos(0).x > SpaceGame.width/3)){
             // Obtenemos el angulo a donde tenemos que girar
             float x1 = this.getX();
@@ -54,17 +63,25 @@ public class Yellow extends Shoot{
                 y2 = SpaceGame.getTouchPos(0).y;
             vector.set(x2-x1,y2-y1);
             // Basta con llamar al vector.angle para tener el angulo a girar
-            // Hay que tener que cuenta que hay que invertir este angulo, es decir
-            // 360 - vector.angle para que esté bien situado
             float angle = vector.angle();
 
+            // Rotamos el rectangulo de colisión y el efecto de particulas
             this.getLogicShape().setRotation(angle);
 
             ParticleEmitter.ScaledNumericValue angles = this.shootEffect.getEmitters().first().getAngle();
             angles.setLow(angle);
-            angles.setHigh(angle-15,angle+15);
+            angles.setHigh(angle-AMPLITUDE_OF_FIRE,angle+AMPLITUDE_OF_FIRE);
         }else{
-            this.changeToDeletable();
+            // Si no, lo borramos
+            //this.changeToDeletable();
+            shootEffect.allowCompletion();
+
+            if(timeToFireAgain < 0){
+                this.changeToDeletable();
+            }else{
+                timeToFireAgain-=delta;
+            }
+
         }
     }
 
