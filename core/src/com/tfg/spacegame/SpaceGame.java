8616cec07f210b9ea8b76b146df847642b7645c0
Joseda13 <joseda.martin@gmail.com>
2016-02-10 21:04:45 +0100

    Cambio en el tipo de fuente
    Se ha cambiado el tipo de fuente a utilizar en el videojuego.

--- core/src/com/tfg/spacegame/SpaceGame.java
+++ core/src/com/tfg/spacegame/SpaceGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Vector3;
 import com.tfg.spacegame.screens.MainMenuScreen;
 import com.tfg.spacegame.utils.AssetsManager;
@@ -19,6 +20,11 @@ public class SpaceGame extends Game {
 	public static int width = 800;
 	public static int height = 480;
 	private static Vector3 touchPos;
+	private FreeTypeFontGenerator generator;
+	private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+
+	public static BitmapFont text;
+	public static BitmapFont title;
 
 	@Override
 	public void create () {
@@ -34,6 +40,14 @@ public class SpaceGame extends Game {
 		touchPos = new Vector3();
 
 		this.setScreen(new MainMenuScreen(this));
+
+		generator = new FreeTypeFontGenerator(Gdx.files.internal("pirulen.ttf"));
+		parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = width/ 44;
+		text = generator.generateFont(parameter);
+		parameter.size = width / 20;
+		title = generator.generateFont(parameter);
+		generator.dispose();
 	}
 
 	public void render() {
