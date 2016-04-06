38204cc5f2b57815c9db64b15bb781900e7e0199
tonigallego <tonigallego87@gmail.com>
2016-04-06 17:45:06 +0200

    Añadidos nuevos archivos a gitignore

---
+++ desktop/src/com/tfg/spacegame/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.tfg.spacegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tfg.spacegame.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
+		config.title = "Space Game";
+		new LwjglApplication(new SpaceGame(null, new DesktopGoogleServices()), config);
+	}
+}
